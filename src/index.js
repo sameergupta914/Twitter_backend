4cffc93da1bc208ea568dbb5a6a13046ce5fff14
Sameer Gupta <sameerguptaa09@gmail.com>
2025-01-22 18:07:50 +0530

    moved to es module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express= require('express');
-const connect=require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 const app=express();
+
+import service from './services/tweet-service.js';
 
 app.listen(3000, async()=>{
     console.log('server started');
     await connect();
     console.log('database connected');
+    let ser= new service();
+    const tweet=ser.create({content: 'heyy #hi'});
     
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,4 +1,4 @@
-const mongoose= require('mongoose');
+import mongoose from 'mongoose';
 
 const tweetSchema= new mongoose.Schema({
     content:{
@@ -14,7 +14,7 @@ const tweetSchema= new mongoose.Schema({
 }, {timestamps: true});
 
 const Tweet= mongoose.model('Tweet', tweetSchema);
-module.exports= Tweet;
+export default Tweet;
 
 // tweetSchema.virtual('contentWithEmail').get(function process(){
 //     return `${this.content} \nCreated by: ${this.userEmail}`;
