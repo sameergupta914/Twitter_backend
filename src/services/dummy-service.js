bc5047a5215057090cfc434357cb776ac3a80fde
Sameer Gupta <sameerguptaa09@gmail.com>
2025-01-28 18:13:48 +0530

    added basic testing

---
+++ src/services/dummy-service.js
@@ -0,0 +1,9 @@
+import {helper} from './helper-service.js';
+export const execute = () => {
+    const result = helper();
+    if(result) {
+        return "Learning JS";
+    } else {
+        return "Learning ReactJS"
+    }
+}
